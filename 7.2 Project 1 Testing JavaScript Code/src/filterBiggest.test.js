c95e289b80c6c6b78faed8e4bf718e857bf78eef
Georgios Feidakis <60877672+geofeid@users.noreply.github.com>
2022-04-25 17:05:50 +0300

    Add files via upload

---
+++ 7.2 Project 1 Testing JavaScript Code/src/filterBiggest.test.js
@@ -0,0 +1,44 @@
+const filterBiggestNumbers = require("./filterBiggest");
+
+// Exercise 5
+test("Will throw if the first parameter is not an array", () => {});
+
+test("Will return correct output for reasonable input", () => {
+  // ✅ Create a test array
+  // ✅ Capture your results
+  // ✅ Create an array for all of the expected values
+  // ✅ Create an array for all of the reject values
+  // ✅ Iterate over last two arrays to confirm that the result array has/doesn't have those values
+
+  const testInput = [4, 2, 5, 3, 7, 8, 10, 12];
+  const got = filterBiggestNumbers(testInput, 7);
+  const want = [8, 10, 12];
+  const rejects = [4, 2, 5, 3, 6];
+
+  want.forEach((value) => expect(got).toContain(value));
+  rejects.forEach((value) => expect(got).not.toContain(value));
+});
+
+test("Will throw if the second argument is not a number", () => {
+  expect(() => filterBiggestNumbers([], "")).toThrowError();
+});
+
+test("Will work if the second number not an integer", () => {
+  const testInput = [4, 2, 5];
+  const got = filterBiggestNumbers(testInput, 4.5);
+  const want = [5];
+  const rejects = [4, 2];
+
+  want.forEach((value) => expect(got).toContain(value));
+  rejects.forEach((value) => expect(got).not.toContain(value));
+});
+
+test("Will work if the min is a negative number", () => {
+  const testInput = [-4, 4, 2, 5];
+  const got = filterBiggestNumbers(testInput, -1);
+  const want = [2, 4, 5];
+  const rejects = [-4];
+
+  want.forEach((value) => expect(got).toContain(value));
+  rejects.forEach((value) => expect(got).not.toContain(value));
+});
